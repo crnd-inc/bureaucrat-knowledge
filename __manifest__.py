7d8c74a481360dfabb1585d3e534a78dc1a4c275
Дмитро Катюха <cto@crnd.pro>
2020-04-23 21:16:39 +0300

    Merge remote-tracking branch 'origin/11.0' into 12.0-forward-port

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
 
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
-    'version': '11.0.0.1.0',
+    'version': '12.0.0.1.0',
     'category': 'Knowledge',
 
     # any module necessary for this one to work correctly

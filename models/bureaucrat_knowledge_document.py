1f50214119ed7253f5ac001360f6552feb0640fa
Дмитро Катюха <cto@crnd.pro>
2020-06-01 14:35:10 +0300

    [FIX] bureaucrat_knowledge: Initial document creation fixed

--- models/bureaucrat_knowledge_document.py
+++ models/bureaucrat_knowledge_document.py
@@ -29,7 +29,11 @@ class BureaucratKnowledgeDocument(models.Model):
     @api.depends('history_ids')
     def _compute_document_latest_history_id(self):
         for record in self:
-            record.latest_history_id = record.history_ids.sorted()[0]
+            if record.history_ids:
+                record.latest_history_id = record.history_ids.sorted()[0]
+            else:
+                record.latest_history_id = self.env[
+                    'bureaucrat.knowledge.document.history'].browse()
 
     @api.depends('latest_history_id', 'latest_history_id.document_body')
     def _compute_document_body(self):
@@ -38,14 +42,18 @@ class BureaucratKnowledgeDocument(models.Model):
 
     def _inverse_document_body(self):
         for record in self:
-            if record.document_body != record.latest_history_id.document_body:
-                self.env['bureaucrat.knowledge.document.history'].create({
-                    'commit_summary': self.commit_summary,
-                    'document_body': self.document_body,
-                    'user_id': self.env.user.id,
-                    'date_create': fields.Datetime.now(),
-                    'document_id': self.id,
-                })
+            if record.document_body == record.latest_history_id.document_body:
+                # There is no sense to create new hisstory record if there is
+                # no changes (document body is same as in prev record)
+                continue
+
+            self.env['bureaucrat.knowledge.document.history'].create({
+                'commit_summary': record.commit_summary,
+                'document_body': record.document_body,
+                'user_id': self.env.user.id,
+                'date_create': fields.Datetime.now(),
+                'document_id': record.id,
+            })
 
     def _search_document_body(self, operator, value):
         return [('latest_history_id.document_body', operator, value)]

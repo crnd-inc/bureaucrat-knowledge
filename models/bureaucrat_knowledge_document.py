cd3ba4b221cf5736c5b94ed7b44e62dbe7092cd9
Дмитро Катюха <cto@crnd.pro>
2020-06-01 14:19:48 +0300

    knowledgebase: improve handling of doc history, UI/UX improvements

--- models/bureaucrat_knowledge_document.py
+++ models/bureaucrat_knowledge_document.py
@@ -13,27 +13,39 @@ class BureaucratKnowledgeDocument(models.Model):
     name = fields.Char(translate=True, index=True, required=True)
     document_body = fields.Html(
         compute='_compute_document_body',
-        inverse='_inverse_document_body')
+        inverse='_inverse_document_body',
+        search='_search_document_body')
     category_id = fields.Many2one(
         'bureaucrat.knowledge.category', index=True, ondelete='restrict')
     history_ids = fields.One2many(
         'bureaucrat.knowledge.document.history', 'document_id')
+    latest_history_id = fields.Many2one(
+        'bureaucrat.knowledge.document.history',
+        compute='_compute_document_latest_history_id',
+        readonly=True, store=True, auto_join=True)
     commit_summary = fields.Char(store=False)
+    active = fields.Boolean(default=True, index=True)
 
-    @api.depends('history_ids', 'history_ids.document_body')
+    @api.depends('history_ids')
+    def _compute_document_latest_history_id(self):
+        for record in self:
+            record.latest_history_id = record.history_ids.sorted()[0]
+
+    @api.depends('latest_history_id', 'latest_history_id.document_body')
     def _compute_document_body(self):
-        for rec in self:
-            history_recs = rec.history_ids.sorted()
-            rec.document_body = (
-                history_recs[0].document_body if history_recs else False)
+        for record in self:
+            record.document_body = record.latest_history_id.document_body
 
     def _inverse_document_body(self):
-        self.env['bureaucrat.knowledge.document.history'].create({
-            'commit_summary': self.commit_summary,
-            'document_body': self.document_body,
-            'user_id': self.env.user.id,
-            'date_create': fields.Datetime.now(),
-            'document_id': self.id,
-        })
+        for record in self:
+            if record.document_body != record.latest_history_id.document_body:
+                self.env['bureaucrat.knowledge.document.history'].create({
+                    'commit_summary': self.commit_summary,
+                    'document_body': self.document_body,
+                    'user_id': self.env.user.id,
+                    'date_create': fields.Datetime.now(),
+                    'document_id': self.id,
+                })
 
-    active = fields.Boolean(default=True, index=True)
+    def _search_document_body(self, operator, value):
+        return [('latest_history_id.document_body', operator, value)]

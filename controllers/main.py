ef097d0235f4421be70895119e2a3414002a031a
Nasalskyi Olexandr <olexandrfec@gmail.com>
2020-07-01 15:21:51 +0300

    Add links to documents

--- controllers/main.py
+++ controllers/main.py
@@ -5,14 +5,14 @@ from odoo.http import request
 class KnowledgeBase(http.Controller):
     @http.route('/knowledge', auth='public', website=True)
     def knowledge_main(self, **kw):
-        return http.request.render(
+        return request.render(
             'bureaucrat_knowledge_website.knowledge_main', {})
 
     @http.route('/knowledge/categories', auth='public', website=True)
     def knowledge_categories(self, **kw):
         Categories = request.env['bureaucrat.knowledge.category']
         cats = Categories.search([])
-        return http.request.render(
+        return request.render(
             'bureaucrat_knowledge_website.knowledge_categories', {
                 'categories': cats})
 
@@ -20,6 +20,21 @@ class KnowledgeBase(http.Controller):
     def knowledge_documents(self, **kw):
         Documents = request.env['bureaucrat.knowledge.document']
         docs = Documents.search([])
-        return http.request.render(
+        return request.render(
             'bureaucrat_knowledge_website.knowledge_documents', {
                 'documents': docs})
+
+    @http.route('/knowledge/documents/<int:doc_id>',
+                auth='public', website=True)
+    def document(self, doc_id, **kw):
+        values = {}
+        docs = request.env['bureaucrat.knowledge.document'].search(
+            [('id', '=', doc_id)])
+
+        if not docs:
+            raise request.not_found()
+
+        values.update({'doc': docs})
+
+        return request.render(
+            'bureaucrat_knowledge_website.knowledge_document', values)

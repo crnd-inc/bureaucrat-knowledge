c23fd94c862bcf5e614174a5e0c4d16b8a3c4d21
Nasalskyi Olexandr <olexandrfec@gmail.com>
2020-07-09 17:44:20 +0300

    Refactor controllers and templates

--- controllers/main.py
+++ controllers/main.py
@@ -1,32 +1,33 @@
+import logging
 from odoo import http
 from odoo.http import request
 
+_logger = logging.getLogger(__name__)
+
 
 class KnowledgeBase(http.Controller):
-    @http.route('/knowledge', auth='public', website=True)
-    def knowledge_main(self, **kw):
-        return request.render(
-            'bureaucrat_knowledge_website.knowledge_main', {})
 
-    @http.route('/knowledge/categories', auth='public', website=True)
-    def knowledge_categories(self, **kw):
+    @http.route(['/knowledge',
+                 '/knowledge/<int:cat_id>',
+                 '/knowledge/<int:cat_id>/<int:subcat_id>'],
+                auth='public', website=True)
+    def knowledge_category(self, cat_id=False, subcat_id=0, **kw):
         Categories = request.env['bureaucrat.knowledge.category']
-        cats = Categories.search([])
-        return request.render(
-            'bureaucrat_knowledge_website.knowledge_categories', {
-                'categories': cats})
-
-    @http.route('/knowledge/documents', auth='public', website=True)
-    def knowledge_documents(self, **kw):
+        if subcat_id:
+            cat_id = subcat_id
+        cats = Categories.search([('parent_id', '=', cat_id)])
         Documents = request.env['bureaucrat.knowledge.document']
-        docs = Documents.search([])
+        docs = Documents.search([('category_id', '=', cat_id)])
+        _logger.warning("Docs : %s", (cats, docs))
         return request.render(
-            'bureaucrat_knowledge_website.knowledge_documents', {
+            'bureaucrat_knowledge_website.knowledge_main', {
+                'categories': cats,
                 'documents': docs})
 
-    @http.route('/knowledge/documents/<int:doc_id>',
+    @http.route(['/knowledge/<int:cat_id>/doc/<int:doc_id>',
+                 '/knowledge/<int:cat_id>/<int:subcat_id>/doc/<int:doc_id>'],
                 auth='public', website=True)
-    def document(self, doc_id, **kw):
+    def document(self, doc_id, cat_id=False, subcat_id=False, **kw):
         values = {}
         docs = request.env['bureaucrat.knowledge.document'].search(
             [('id', '=', doc_id)])

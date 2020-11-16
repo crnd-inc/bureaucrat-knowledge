4e218b98903533f3171c921a44dfc4a3446153e5
Dmytro Katyukha <firemage.dima@gmail.com>
2020-11-16 20:00:40 +0200

    Fix versions

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
 
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
-    'version': '12.0.0.10.3',
+    'version': '12.0.0.11.1',
     'category': 'Knowledge',
 
     # any module necessary for this one to work correctly

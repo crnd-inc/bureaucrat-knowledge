ec8ba7c851585a524442ec08765407451268ecc0
Nasalskyi Olexandr <olexandrfec@gmail.com>
2021-01-22 14:57:46 +0200

    Migration work

--- migrations/12.0.0.13.0/post-migrate.py
+++ migrations/12.0.0.13.0/post-migrate.py
@@ -3,10 +3,14 @@ def migrate(cr, installed_version):
         UPDATE bureaucrat_knowledge_document AS bnd
         SET document_type = 'html'
         WHERE document_type is null;
-    """)
 
-    cr.execute("""
         UPDATE bureaucrat_knowledge_document_history AS bnd
         SET document_type = 'html'
         WHERE document_type is null;
+
+        UPDATE bureaucrat_knowledge_document
+        SET document_body_html = temporary_document_body;
+
+        ALTER TABLE bureaucrat_knowledge_document
+        DROP COLUMN temporary_document_body;
     """)

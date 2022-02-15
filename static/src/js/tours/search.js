88a0791522b01423a4418436d296810515391dc3
AShurikas <ANechipor@gmail.com>
2022-02-15 18:21:38 +0200

    - fix linter

--- static/src/js/tours/search.js
+++ static/src/js/tours/search.js
@@ -9,38 +9,39 @@ odoo.define('bureaucrat_knowledge_website.search', function (require) {
     }, [
         {
             content: 'Navigate to knowledge base page',
-            trigger: "#top_menu > li:nth-child(3) > a > span"
+            trigger: "#top_menu > li:nth-child(3) > a > span",
         },
 
         {
             content: "Fill search text 'Generic' in searchbar",
             trigger: "form#wsd-knowledge-search input[name='search']",
-            run: "text Generic"
+            run: "text Generic",
         },
         {
             content: "Click on 'search' button",
-            trigger: ".btn.btn-outline-secondary"
+            trigger: ".btn.btn-outline-secondary",
         },
         {
             content: "Check that specific article was found",
-            trigger: ".child_doc [data-oe-model='bureaucrat.knowledge.document']",
+            trigger: ".child_doc" +
+                " [data-oe-model='bureaucrat.knowledge.document']",
         },
         {
             content: 'Waiting for result',
-            trigger: "#genenric-assignment-hr"
+            trigger: "#genenric-assignment-hr",
         },
         {
             content: "Navigate back to root of knowledge base",
-            trigger: ".breadcrumb-item a[href='/knowledge/']"
+            trigger: ".breadcrumb-item a[href='/knowledge/']",
         },
         {
             content: "Try to search for unexisting article",
             trigger: "form#wsd-knowledge-search input[name='search']",
-            run: "text There are no documents must be found"
+            run: "text There are no documents must be found",
         },
         {
             content: "Click on 'search' button",
-            trigger: ".btn.btn-outline-secondary"
+            trigger: ".btn.btn-outline-secondary",
         },
     ]);
     return {};

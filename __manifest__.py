54c0469a065854668fa73ae9f1e2456fc9280e04
AShurikas <ANechipor@gmail.com>
2022-02-22 20:32:10 +0200

    - fix versions

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,9 @@
 
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
-    'version': '12.0.0.11.0',
+
+    'version': '12.0.0.12.0',
+
     'category': 'Knowledge',
 
     # any module necessary for this one to work correctly

e4a8149fe08734188f5664382a9db294c2a1da77
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-08-10 13:30:11 +0300

    [FIX] fix migration + add changelogs

--- bureaucrat_knowledge/migrations/12.0.0.28.0/pre-migrate.py
+++ bureaucrat_knowledge/migrations/12.0.0.28.0/pre-migrate.py
@@ -1,7 +1,7 @@
 from odoo.addons.generic_mixin.tools.migration_utils import ensure_version
 
 
-@ensure_version('0.30.0')
+@ensure_version('0.28.0')
 def migrate(cr, installed_version):
     cr.execute("""
         ALTER TABLE bureaucrat_knowledge_document
